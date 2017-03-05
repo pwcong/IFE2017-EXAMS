51406b3ce4143e2f505138d4d7d6de951bb3c1c5
pwcong <pwcong@foxmail.com>
2017-03-06 00:13:15 +0800

    [+] prepare music-player -> display cover

--- src/Front-End/music-player/src/view/app.js
+++ src/Front-End/music-player/src/view/app.js
@@ -1,12 +1,16 @@
-import React from "react";
-import style from "./style/app.css";
-
+import React from 'react';
+import style from './style/app.css';
+import { connect } from 'react-redux';
 
 // http://www.0dutv.com/plug/down/up2.php/97758.mp3
-export default class App extends React.Component{
+class App extends React.Component{
 
     constructor(props){
         super(props);
+
+        this.state = {
+            isPlaying: true
+        };
     }
 
     render(){
@@ -14,7 +18,25 @@ export default class App extends React.Component{
         return (
 
             <div className={style.root}>
-                
+                <div className={style.console}>
+                    <div className={style['console-cover']}>
+                        <div 
+                            className={style['cover-bg']}
+                            style={{
+                                backgroundImage: 'url(http://p3.music.126.net/MHIvytC5RXh5Lp2J_3tpaQ==/19017153114022258.jpg)'
+                            }}
+                            >
+                        </div>
+                        <div 
+                            className={ style.cover + ' ' + (this.state.isPlaying ? 'rotate' : '')}
+                            style={{
+                                backgroundImage: 'url(http://p3.music.126.net/MHIvytC5RXh5Lp2J_3tpaQ==/19017153114022258.jpg)'
+                            }}>
+                        </div>
+                    </div>
+                    <div className={style['console-main']}></div>
+                </div>
+                <div className={style['song-list']}></div>
             </div>
 
         );
@@ -22,5 +44,12 @@ export default class App extends React.Component{
 
     }
 
-
 }
+
+function select(state){
+    return ({
+
+    })
+}
+
+export default connect()(App);
